281ef27d4a62def2aafa7cd7f1721799de71bcaa
Cedric Che-Azeh <cedricipkiss@gmail.com>
2025-06-19 18:18:42 +0100

    modified signin to include phone number and email

--- signin.php
+++ signin.php
@@ -1,84 +1,85 @@
 <?php
-require_once __DIR__."/site-variables.php";
+require_once __DIR__ . "/site-variables.php";
+// require __DIR__ . '/vendor/autoload.php';
 
-Class Endpoint extends API {
-    
-    public function __construct() {
-        parent::__construct();
-    }
+// use Twilio\Rest\Client;
 
-    public function load_data() {
-        $this->query(
-            "SELECT id FROM users WHERE tel = ? AND stat > ?",
-            array($_POST["tel"], 0),
-            "user"
-        );
-    }
+class Endpoint extends API {
 
-    private function save_otp($otp, $id) {
-      $this->query(
-        "UPDATE users SET otp = ?, otp_time = NOW() WHERE id = ?",
-        array($otp, $id),
-        "otp"
-      );
-    }
+	public function __construct() {
+		parent::__construct();
+	}
 
-    private function save_new_user($tel,$otp) {
-      $this->query(
-        "INSERT INTO users (tel,first_name,last_name,otp,country,otp_time) VALUES(?,?,?,?,?, NOW())",
-        array($tel, "No Name", "", $otp, $_POST['country']),
-        "new_user"
-      );
-    }
+	public function load_data() {
+		$this->query(
+			"SELECT id FROM users WHERE (tel = ? OR email = ?) AND stat > ?",
+			array($_POST["tel"], $_POST['email'], 0),
+			"user"
+		);
+	}
 
-    public function get_content() {
-      if(empty($_POST)) {
-        $this->respond(array(
-          "error" => 'No sigin credentials specified'
-          )
-        );
-      }
-        $this->load_data();
-        if (empty($this->query_data["user"]["stat"])) {
+	private function save_otp($otp, $id) {
+		$this->query(
+			"UPDATE users SET otp = ?, otp_time = NOW() WHERE id = ?",
+			array($otp, $id),
+			"otp"
+		);
+	}
 
-            // Generate OTP
-            $otp = random_int(1000, 9999);
+	private function save_new_user($tel, $email, $otp) {
+		$this->query(
+			"INSERT INTO users (stat,tel,email,first_name,otp,otp_time,last_name) VALUES(?,?,?,?, NOW(),?)",
+			array(0, $tel, $email, "No Name", $otp, ""),
+			"new_user"
+		);
+	}
 
-            // Send Emailemail
-            // $this->send_email(
-            //     "Your login code is $otp",
-            //     "Your Court Scheduler Login Code",
-            //     "Court Scheduler PLC <admin@scrm-cloud.net>",
-            //     $_POST['email']
-            // );
+	public function get_content() {
+		$this->load_data();
+		if (empty($this->query_data["user"]["stat"])) {
 
-            // Send SMS
-            // A Twilio number you own with SMS capabilities
-            $twilio_number = "+19049002981";
+      		// Generate OTP
+			$otp = random_int(1000, 9999);
+			$response = '[]';
 
-            $client = new Client(SMS_ID, SMS_TOKEN);
-            $to = "+" . ltrim($_POST['tel'], '+');
+			if ($_POST['ismobileLogin'] === true) {
+        		
+        		// Logging in with phone
+				$phoneNumber = $_POST["tel"];
+				$smsMessage = $otp . '-PHPVISA';
+				$url = "https://smsvas.com/bulk/public/index.php/api/v1/sendsms?user=info@phpvisa.com&password=biometrie2023&senderid=PHPVISA&mobiles={$phoneNumber}&sms={$smsMessage}";
 
-            $client->messages->create(
-                // Where to send a text message (your cell phone?)
-                $to,
-                array(
-                    'from' => $twilio_number,
-                    'body' => "Your Waste2Rewards login code is $otp"
-                )
-            );
+				$ch = curl_init();
+				curl_setopt($ch, CURLOPT_URL, $url);
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-            if ( !empty($this->query_data["user"]["data"]) ) {
+        		// Execute the cURL request
+				$response = curl_exec($ch);
+        		// Close cURL session to free up resources
+				curl_close($ch);
+			} else {
+				// Logging in with email
+				$this->send_email(
+				    "Your login code is $otp",
+				    "Your PHPVisa Login Code",
+				    "PHPVisa <phpvisa@maxwellsecure.net>",
+				    $_POST['email']
+				);
 
-              // Save OTP
-              $this->save_otp($otp, $this->query_data["user"]["data"][0]["id"]);
-              $this->respond(array("user" => $this->query_data["user"]["data"][0]["id"]));
-            } else {
-              
-              // User doesn't exist, create them and send OTP
-              $this->save_new_user($_POST['tel'], $otp);
-              $this->respond(array("user" => $_SESSION['lastInsertId']));
-            }
-        }
-    }
+			}
+
+
+			if (!empty($this->query_data["user"]["data"])) {
+
+        		// Save OTP
+				$this->save_otp($otp, $this->query_data["user"]["data"][0]["id"]);
+				$this->respond(array("user" => $this->query_data["user"]["data"][0]["id"], 'response' => json_decode($response)));
+			} else {
+
+        		// User doesn't exist, create them and send OTP
+				$this->save_new_user($_POST["tel"], $otp);
+				$this->respond(array("user" => $_SESSION['lastInsertId'], 'response' => json_decode($response)));
+			}
+		}
+	}
 }
